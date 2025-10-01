3c9fdb2526b1fa8a278d495580a112d699cf7c2b
SENZHO0 <syufu3@gmail.com>
2025-10-02 00:10:49 +0500

    Delete Prime_Number.py

--- Prime_Number.py
+++
@@ -1,8 +0,0 @@
-for number in range(25, 50):
-    prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            prime = False
-            break
-    if prime:
-        print(number, end=" ")
